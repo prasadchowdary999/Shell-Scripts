2bb994a81414dbcf637fdf6184ac0936f0e14b2d
Prasad <prasadchowdarybotla104@gmail.com>
2025-09-07 21:59:38 +0530

    changed

---
+++ 3.Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+A=Rakesh
+B=Priya
+
+
+echo "A:: Hello B, How are you?"
+echo "B:: Hi A, I am fine. How are you?"
+echo "A: I am fine too. how is your work?"
+echo "B:: not bad. I am thinking to upgrade to DevOps"
